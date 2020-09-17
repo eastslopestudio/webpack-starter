ee23f79df2c2e992962543dc4d382569445d8f83
Ryan Scherler <ryan@eastslope.studio>
2020-09-17 14:26:44 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# webpack-starter
+# Webpack Starter
+
 A starter setup for front-end development using Webpack.
+
+## Get up and running
+
+```sh
+npm install
+npm start # aliases npm run watch
+npm run dev 
+npm run prod
+```
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path')
+const webpack = require('webpack')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, './assets/js/site.js'),
+    path.resolve(__dirname, './assets/scss/site.scss'),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'site.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({})],
+  },
+  plugins: [
+    new ProgressBarPlugin(),
+    new CleanWebpackPlugin(),
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'site.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './assets/images'),
+          to: 'images',
+          force: true,
+          globOptions: {
+            ignore: ['**/.gitkeeps*'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+    new ImageminPlugin.default({
+      disable: process.env.NODE_ENV !== 'production',
+      test: /\.(jpe?g|png|gif|svg)$/i,
+    }),
+  ],
+  mode: process.env.NODE_ENV,
+}
